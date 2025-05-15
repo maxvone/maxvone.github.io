7949901db693626253e519d629b83e77c869620d
Max Vone <maximvone@gmail.com>
2025-05-15 10:42:07 +0200

    return base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import bookshop from '@bookshop/astro-bookshop';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://maxvone.github.io',
+  base: '/personal-page',
   integrations: [bookshop(), react()]
 });
